bb2c5977be716b10de5b44970464be4784e37b1c
mercury <udtrokia@163.com>
2018-03-24 14:45:04 +0800

    backups

--- init.el
+++ init.el
@@ -90,9 +90,9 @@
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
 ; create the autosave dir if necessary, since emacs won't.
-(make-directory "~/.emacs.d/autosaves/" t)
-;;(make-directory "~/.emacs.d/backups/" t)
+;;(make-directory "~/.emacs.d/autosaves/" t)
+(make-directory "~/.emacs.d/backups/" t)
 ; put files
 (custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t))))
-;;  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+;;  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t))))
+  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
