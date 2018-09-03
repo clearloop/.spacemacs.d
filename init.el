b25685a0197ee0cc2eda9fe5ecf945d4e90c71e7
mercury <udtrokia@gmail.com>
2018-09-03 23:30:45 +0800

    transparent status bar

--- init.el
+++ init.el
@@ -32,7 +32,11 @@
 ;; (put 'downcase-region 'disabled nil)
 
 ;; <Variables>
+;;(menu-bar-mode 0)
+;;(set-face-background 'mode-line "transparent")
 (custom-set-variables
+ (menu-bar-mode 0)
+ (set-face-background 'mode-line "transparent")
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
@@ -48,7 +52,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (menu-bar-mode 0)
  )
 
 ;; paths
