4c3bf916e918a3370863d518916604feeb9a9975
clearloop <udtrokia@gmail.com>
2020-04-05 20:07:23 +0800

    update: delete verbose default settings

---
+++ layers/config/packages.el
@@ -0,0 +1,37 @@
+;;; packages.el --- cydonia layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Mercury <mercury@Marianne.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `cydonia-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `cydonia/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `cydonia/pre-init-PACKAGE' and/or
+;;   `cydonia/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+
+(defconst config-packages '(
+                            python
+                            ivy
+                            emacs-lisp))
+;;; packages.el ends here
